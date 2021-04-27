39deb767603b6081883f4b72f3436803657df000
Birk Thestrup Blauner <birkblauner@gmail.com>
2021-04-27 18:23:55 +0200

    role creation

---
+++ btb-forum.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * BTB Forum
+ *
+ * @package           PluginPackage
+ * @author            Birk Thestrup Blauner
+ * @copyright         2019 Your Name or Company Name
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       BTB Forum
+ * Description:       Add new user role, which can access private pages. Also add a Forum post type, where users can create new posts.
+ * Version:           beta
+ * Author:            Birk Thestrup Blauner
+ * Text Domain:       btb-forum
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+
+defined('ABSPATH') or die('Nothing to do here...');
+
+class BtbForum {
+
+    function __construct() {
+        add_action( 'init', array($this, 'add_forum_member_role') );
+        add_action( 'init', array($this, 'custom_post_type') );
+    }
+
+    function activate() {
+        //generate a CPT
+        $this->add_forum_member_role();
+        $this->custom_post_type();
+        //generate a role
+        //generate a plugin page with fields
+        // flush rewrite rules
+        flush_rewrite_rules();
+    }
+
+    function deactivate() {
+
+        remove_role('forum_member');
+        //flush rewrite rules
+        flush_rewrite_rules();
+
+    }
+
+    function uninstall() {
+        //delete cpt
+        //delete role
+        //delete fields
+
+        //delete all plugin data from db
+    }
+
+    function add_forum_member_role() {
+        add_role( 
+            'forum_member',
+            __('Forum Member', 'btb-forum'),
+            array(
+                'delete_posts'          => true,
+                'edit_posts'            => true,
+                'read'                  => true,
+                'read_private_pages'    => true,
+                'read_private_posts'    => true,
+            ), 
+        );
+    }
+
+    function custom_post_type() {
+        register_post_type( 'book', ['public' => true, 'label' => 'books'] );
+    }
+}
+
+if (class_exists('BtbForum')) {
+    $btbForum = new BtbForum();
+}
+
+// Activation
+register_activation_hook( __FILE__, array($btbForum, 'activate') );
+
+// Deactivation
+register_deactivation_hook( __FILE__, array($btbForum, 'deactivate') );
+
+// Unistall
